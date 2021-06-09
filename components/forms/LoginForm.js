9f32eff9a962e98abc4fd0d9b9fd71d68ba06a57
Alex <alex.beciana@gmail.com>
2021-06-09 00:17:23 -0400

    Restyled login page with grids

--- components/forms/LoginForm.js
+++ components/forms/LoginForm.js
@@ -12,7 +12,7 @@ const LoginForm = () => {
         <React.Fragment>
             {/* <h1 className="text-5xl">Login</h1> */}
             {/* <div className="bg-gray-600 w-4/12 m-auto"> */}
-            <div className="w-10/12 md:w-4/12">
+            <div className="w-12/12">
                 <div style={{"zIndex": "3", "backgroundColor": "#EFEFEF", "margin": "auto"}}>
                     <form className="p-10 md:p-14 lg-p-10" onSubmit={submitHandler}>
                         <label>Email:</label>
@@ -20,7 +20,7 @@ const LoginForm = () => {
                         <input
                             type="email"
                             // className="form-input rounded-full mt-2 w-4/5 py-3 px-4 block border-#EAE9EC"
-                            className="mt-2 block w-full py-3 px-4 rounded-md border-black focus:outline-none"
+                            className="mt-2 block w-full py-3 px-4 rounded-md border-black focus:outline-none w-4/5"
                             placeholder="Email"
                         />
                         <br/>
@@ -29,7 +29,7 @@ const LoginForm = () => {
                         <input
                             type="password"
                             // className="form-input rounded-full mt-1 w-full py-3 px-4 block border-#EAE9EC"
-                            className="mt-2 block w-full py-3 px-4 rounded-md border-red-500 focus:outline-none"
+                            className="mt-2 block w-full py-3 px-4 rounded-md border-red-500 focus:outline-none w-4/5"
                             placeholder="Password"
                         />
                         {/* <input type="email" class="form-input px-4 py-3 rounded-full"/> */}
--- pages/login.js
+++ pages/login.js
@@ -9,15 +9,16 @@ const Login = () => {
         <Head>
             <title>Compil | Login</title>
         </Head>
-        <section className="flex flex-col sm:flex-wrap md:flex-row items-center justify-evenly">
+        {/* <section className="flex flex-col sm:flex-wrap md:flex-row items-center justify-evenly"> */}
+        <section className="grid grid-cols-1 lg:grid-cols-3 mx-8 mt-10 gap-1">
         {/* <section className="grid grid-cols-3 place-items-center place-content-center"> */}
-            <aside className="flex text-9xl text-white text-left md:text-center hidden md:inline lg:transform lg:-rotate-90 justify-self-start">Login</aside>
+            <aside className="text-9xl text-white text-left md:text-center hidden lg:inline lg:transform lg:-rotate-90 justify-self-center ml-20 mb-3">Login</aside>
             {/* <div className="text-center"> */}
-            <h1 className="text-5xl inline sm:hidden">Login</h1>
+            <aside className="text-8xl md:text-9xl text-white text-center inline lg:hidden">Login</aside>
             <img
                 src={"/static/images/music-login.svg"}
                 alt="Login Screen"
-                className="w-10/12 md:w-5/12 lg:w-5/12"
+                className="w-12/12 md:-ml-2 lg:-ml-48 justify-self-center place-self-center"
                 // className="w-full"
             />
             {/* </div> */}
