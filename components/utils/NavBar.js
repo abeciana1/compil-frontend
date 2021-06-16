15403c163ab7337c373c9a881c1fbe8c8ce8caab
Alex <alex.beciana@gmail.com>
2021-06-15 22:28:15 -0400

    Removed commented out code

--- components/utils/NavBar.js
+++ components/utils/NavBar.js
@@ -63,25 +63,6 @@ const NavBar = (props) => {
                         </Link>
                         </>
                         }
-                        {/* {navigation.map((item, itemIdx) =>
-                        itemIdx === 0 ? (
-                        <Fragment key={item}> */}
-                            {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                            {/* hover:bg-gray-700 hover:text-#E54B4B */}
-                            {/* <Link href={"/" + item.toLowerCase()} className="bg-#28262C-900 text-#28262C px-3 py-2 rounded-md text-sm font-medium">
-                            {item}
-                            </Link>
-                        </Fragment>
-                        ) : ( */}
-                        {/* <Link
-                            key={item}
-                            href={"/" + item.toLowerCase()}
-                            className="text-#28262C-300 px-3 py-2 rounded-md text-sm font-medium"
-                        >
-                            {item}
-                        </Link>
-                        )
-                        )} */}
                     </div>
                 </div>
                 </div>
