7ac2bf0cbb8c8735e0b933dd71b9985e0e75ef24
Alex <alex.beciana@gmail.com>
2021-08-12 00:56:46 -0400

    Began playlist track render

--- components/cards/TrackListItem.js
+++ components/cards/TrackListItem.js
@@ -2,10 +2,70 @@ import React from 'react';
 
 const TrackListItem = (props) => {
 
+    console.log(props);
+
+    const {track} = props;
 
     return (
         <React.Fragment>
-
+            <li
+                className="border hover:bg-black hover:text-white"
+            >
+                <div
+                    className="track"
+                >
+                    <img
+                        // src={track.snippet.thumbnails.default.url}
+                        className="h-10 w-10"
+                    />
+                    <div
+                        className="md:self-center -ml-16 md:ml-5 grid"
+                    >
+                        {track.title}
+                        <div>
+                            {track.artist}
+                        </div>
+                        <div>
+                            <span className="self-center float-left pt-3 relative mr-5 block md:hidden">
+                                {track.album}
+                            </span>
+                            <span className="self-center float-right pt-3 relative mr-5 block md:hidden">
+                                <a
+                                    // href={"https://youtu.be/" + track.contentDetails.videoId}
+                                    target="_blank"
+                                    rel="noopener"
+                                >
+                                    YouTube Link
+                                </a>
+                                <a
+                                    className="ml-5 cursor-pointer"
+                                    // onClick={}
+                                >
+                                    Remove song
+                                </a>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+                <span className="self-center text-center relative bottom-8 mr-5 hidden md:block">
+                    {track.album}
+                </span>
+                <span className="self-center float-right relative bottom-14 mr-5 hidden md:block">
+                    <a
+                        // href={"https://youtu.be/" + track.contentDetails.videoId}
+                        target="_blank"
+                        rel="noopener"
+                    >
+                        YouTube Link
+                    </a>
+                    <a
+                        className="ml-5 cursor-pointer"
+                        // onClick={}
+                    >
+                        Remove song
+                    </a>
+                </span>
+            </li>
         </React.Fragment>
     )
 }
--- components/containers/TrackListing.js
+++ components/containers/TrackListing.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
-import TracklistItem from '../cards/TracklistItem'
+import TracklistItem from '../cards/TrackListItem'
 
 const TrackListing = (props) => {
 
     return (
         <React.Fragment>
-
+            {props.songs ?
+            <section
+                className="pt-5"
+            >
+                <ul>
+                    {props.songs.map((track) => {
+                        return (
+                        <TracklistItem track={track} />
+                        )
+                    })}
+                </ul>
+            </section>
+            : null}
         </React.Fragment>
     )
 }
