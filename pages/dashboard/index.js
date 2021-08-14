d159eaaf9b4edf2a52e17bc3c2946c5a864c2017
Alex <alex.beciana@gmail.com>
2021-08-14 13:09:07 -0400

    Fixed dashboard button rendering to grid css

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -9,6 +9,20 @@ import { useRouter } from 'next/router'
 //* utils
 import PageMargin from '../../components/utils/PageMargin'
 
+// TODO Add sorting for user playlists -
+    // TODO chronologically
+    // TODO reverse chronologically
+    // TODO title
+
+const sortUserPlaylistsByDate = (playlists) => {
+
+    let sorted = playlists.sort((a, b) => {
+        let dateA = new Date(a.created_at)
+        let dateB = new Date(b.created_at)
+        return dateA - dateB
+    })
+}
+
 const Dashboard = (props) => {
 
     const { user } = props
@@ -28,15 +42,17 @@ const Dashboard = (props) => {
                 <PageMargin>
                     <h1 className="text-6xl">Hello, {user.currentUser.f_name.charAt(0).toUpperCase() + user.currentUser.f_name.substr(1).toLowerCase()}!</h1>
                     <div className="lg:ml-96 pt-10 lg:pt-5 pb-5">
-                        <div className="text-6xl flex flex-wrap md:flex-row items-stretch">
+                        {/* <div className="text-6xl flex flex-wrap md:flex-row items-stretch"> */}
+                        <div className="text-6xl grid grid-cols-1 md:grid-cols-3 items-stretch mx-auto xl:flex">
                             Playlists 
-                            <span className="text-lg lg:text-3xl self-center text-center pt-2 lg:pt-0 sm:pl-5">
+                            <span className="text-lg self-center text-center pt-2 lg:pt-0 justify-self-start lg:justify-self-center xl:pl-10">
                                 <button
                                     className="flex items-stretch border-2 pl-1.5 pr-2 pt-0.5 pb-0.5 lg:pl-3 lg:pr-5 lg:pt-1 lg:pb-1 rounded-full focus:outline-none"
                                     style={{ "backgroundColor": "#E54B4B", "color": "#FFFDF3" }}
                                     onClick={() => setModal(!modal)}
                                 >
-                                    <span className="float-right pr-1 lg:pr-2 self-center">
+                                    {/* <span className="float-right pr-1 lg:pr-2 self-center"> */}
+                                    <span className="pr-1 lg:pr-2 self-center">
                                         <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 lg:h-6 lg:w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 6v6m0 0v6m0-6h6m-6 0H6" />
                                         </svg>
@@ -44,7 +60,7 @@ const Dashboard = (props) => {
                                     Create a playlist
                                 </button>
                             </span>
-                            <span className="text-lg lg:text-3xl self-center text-center pt-2 lg:pt-0 sm:pl-5">
+                            <span className="text-lg self-center text-center pt-2 lg:pt-0 justify-self-start lg:justify-self-center xl:pl-10">
                                 <button className="flex items-stretch border-2 pl-2 pr-2 pt-0.5 pb-0.5 lg:pl-5 lg:pr-3 lg:pt-1 lg:pb-1 rounded-full focus:outline-none" style={{"backgroundColor": "#9FC2CC", "color": "#FFFDF3"}}>
                                     See all playlists
                                     <span className="float-right pl-1 lg:pl-2 self-center">
@@ -78,7 +94,8 @@ const Dashboard = (props) => {
                     </section>
                     <section className="grid block grid-flow-col auto-cols-min gap-x-5 lg:ml-96 lg:gap-x-10 pb-10 overflow-x-scroll">
                     {/* <section className="grid hidden lg:block grid-flow-col auto-cols-min ml-96 gap-x-10 pb-10"> */}
-                            {user.currentUser.playlists.map(playlist => {
+                            {sortUserPlaylistsByDate(user.currentUser.playlists)}
+                            {(user.currentUser.playlists).map(playlist => {
                             return (
                             <>
                                 <PlaylistSquare playlist={playlist} />
