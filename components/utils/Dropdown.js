48e85f56f177291116c69604d9f2d699ff98785d
Alex <alex.beciana@gmail.com>
2021-09-20 19:40:20 -0400

    Fixed positioning for dropdown selection icon

--- components/utils/Dropdown.js
+++ components/utils/Dropdown.js
@@ -25,7 +25,7 @@ const Dropdown = (props) => {
                 >
                     <div className="flex items-center ml-5 justify-items-stretch capitalize">
                         {defaultVal}
-                        <span className="ml-48">
+                        <span className="absolute left-52 lg:left-64">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
                             </svg>
